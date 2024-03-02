f5391c826e250d7acfe41ca6b2ef7b69a8b1969c
Nao masa <yamanao0105@gmail.com>
2024-03-02 12:25:26 +0900

    Third commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,7 +59,7 @@ const IndexPage: NextPage<Props> = ({initialImageUrl}) => {
     return (
     <div className={styles.page}>
         <button onClick={handleClick} className={styles.button}>
-            See Other kitten!!
+            One More Kitten!!
         </button>
         <div className={styles.frame}>
             {loading || <img src={imageUrl} className={styles.img} />}
