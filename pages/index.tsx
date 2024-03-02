6d8b2f58e4ba27fd9a9b35e352d8fbca679bd015
Nao masa <yamanao0105@gmail.com>
2024-03-02 12:20:07 +0900

    Second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,71 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { GetServerSideProps, NextPage } from "next";
+import { useEffect, useState } from "react";
+import styles from "./index.module.css";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+type Props = {
+    initialImageUrl: string;
+};
+
+type Image = {
+    url: string;
+};
+
+const isImage = (value: unknown): value is Image => {
+    if (!value || typeof value !== "object") {
+        return false;
+    }
+    return "url" in value && typeof value.url === "string";
+};
+
+const fetchImage = async(): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images: unknown = await res.json();
+    console.log(images);
+    // Array?
+    if (!Array.isArray(images)) {
+        throw new Error("Not able to get images.");
+    }
+    const image: unknown = images[0];
+    if (!isImage(image)) {
+       throw new Error("Not able to get image obj."); 
+    }
+    return image;
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async() => {
+    const image = await fetchImage();
+    return {
+        props: {
+            initialImageUrl: image.url,
+        },
+    };
+};
+
+const IndexPage: NextPage<Props> = ({initialImageUrl}) => {
+    const [imageUrl, setImageUrl] = useState(initialImageUrl);
+    const [loading, setLoading] = useState(false);
+    // useEffect(() => {
+    //     fetchImage().then((newImage) => {
+    //         setImageUrl(newImage.url);
+    //         setLoading(false);
+    //     });
+    // }, []);
+    const handleClick = async() => {
+        setLoading(true);
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url);
+        setLoading(false);
+    };
+    return (
+    <div className={styles.page}>
+        <button onClick={handleClick} className={styles.button}>
+            See Other kitten!!
+        </button>
+        <div className={styles.frame}>
+            {loading || <img src={imageUrl} className={styles.img} />}
+        </div>
+    </div>
+    );
+};
 
 export default IndexPage;
